4f7b756208db0169dd31892edce39c71216cde68
Duong1009 <duongb2014559@student.ctu.edu.vn>
2023-11-22 01:19:19 +0700

    backend buoi2

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -3,6 +3,30 @@ class ContactService {
     constructor(client) {
         this.contact = client.db().collection("contacts");
     }
+
+    extractContactData(payload) {
+        const contact = {
+            name: payload.name,
+            email: payload.email,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+
+        Objects.key(contact).forEach(
+            (key) => contact[key] === undefined && delete contact[key]
+        );
+        return contact;
+    }
+
+    async create(payload) {
+        const contact = this.extractContactData(payload);
+        const result = await this.contact.findOneAndUpdate(
+            contact, 
+            {$set: {favorite: contact.favorite === true}},
+            { returnDocument: "after", upsert: true}
+        );
+    }
 }
 
 module.exports = ContactService;
